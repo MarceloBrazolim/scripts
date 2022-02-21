75e04f9d377149333f55e98eb8c6c007286a07d5
Marcelo Brazolim <marcelobrazolim@gmail.com>
2022-02-21 01:01:53 -0300

    a

--- dev-map.sh
+++ dev-map.sh
@@ -180,13 +180,16 @@ if [ $s ]; then
   ip_self=$(ip address | grep -e "inet " | cut -d '/' -f 1 | cut -d "t" -f 2 | cut -d '.' -f 1,2,3)
 
   if [ $hd ]; then
-    [ $v ] && (echo "  Discovering hosts..")
+    [ $v ] && (echo "Discovering hosts..")
     whytho=("${target[@]:1}")
     var=$(printf %s "${target[0]}" "${whytho[0]/#/.}")
     [ $v ] && (echo "Targeting: (${var[@]}.0-${range1}.0-${range})")
-    for (( i= 0; i <= $range1; i++ )); do
-      (ping -W $W -c $c $var${i} | grep "bytes from" >> $LOCK_FILE &) 2>&-;
+    for (( i = 0; i <= $range1; i++ )); do
+      for (( y = 0; y <= 1; y++ )); do
+        (ping -W $W -c $c $var.${i}.${y} | grep "bytes from" >> $LOCK_FILE &) 2>&-;
+      done
     done
+    [ $ v ] && (echo "")
   fi
 
   echo "  Select which connection to map:"
